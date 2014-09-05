b448813e44762a24772cda4037751b43c0ba25a6
Jon Ong <jonongjs@gmail.com>
2014-09-05 10:25:36 +0800

    Fixed opening empty tab after cancelling file dialog.

--- js/InputChangeDirective.js
+++ js/InputChangeDirective.js
@@ -2,10 +2,12 @@ app.directive('inputChange', function($window) {
 	return function(scope, element, attrs) {
 		element[0].onchange = function(e) {
 			var files = e.target.files || e.dataTransfer.files;
-			scope.f = files[0];
-			 scope.$apply(function(){
-				 scope.$eval(attrs.inputChange)
-			 });
+			if (files.length > 0) {
+				scope.f = files[0];
+				scope.$apply(function(){
+					scope.$eval(attrs.inputChange)
+				});
+			}
 		};
 	}
 });

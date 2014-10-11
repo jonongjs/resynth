5d0c34e1bae9ff848aa640b433bdcd07df9090ca
Jon Ong <jonongjs@gmail.com>
2014-10-11 13:54:19 +0800

    Fixed file input field bug where the same file could not be loaded twice in a row.

--- js/InputChangeDirective.js
+++ js/InputChangeDirective.js
@@ -2,6 +2,10 @@ app.directive('inputChange', function($window) {
 	return function(scope, element, attrs) {
 		scope.uploader = element[0];
 
+		var resetFormElement = function(e) {
+			angular.element(e).parent()[0].reset();
+		};
+
 		element[0].onchange = function(e) {
 			var files = e.target.files || e.dataTransfer.files;
 			if (files.length > 0) {
@@ -10,6 +14,8 @@ app.directive('inputChange', function($window) {
 					scope.$eval(attrs.inputChange)
 				});
 			}
+
+			resetFormElement(element[0]);
 		};
 	}
 });
